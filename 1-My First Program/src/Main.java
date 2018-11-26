768a9f0eddf0f42d5e6854f115ac863adaf80c57
nominbold <nominboldbaatar@gmail.com>
2018-11-26 00:32:12 -0500

    JavaProject

---
+++ 1-My First Program/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Mike Smith");
+		System.out.print("How are you?");
+		System.out.println("I'm good, thanks");
+		System.out.println("Bye.Class is finished");
+		
+		
+		
+		
+	}
+	
+
+}
